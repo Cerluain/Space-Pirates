1388343247546ab41799ab023cbc8372ded73d05
Cerluain <50966861+Cerluain@users.noreply.github.com>
2024-12-19 20:50:22 -0700

    Some basic changes
    Normal Weapons Scripts and normal bullets.

---
+++ Space Pirates/Assets/Scripts/Weapon Scripts/NormalWeaponScript.cs
@@ -0,0 +1,31 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalWeaponScript : MonoBehaviour
+{
+    public int damage = 10;
+    public int material_cost = 20;
+    public int shot_count = 30;
+    public int speed = 10;
+    //public Object bullet_prefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShootWeapon()
+    {
+        //Creates a bullet prefab and passes weapon properties into the bullet shot
+
+    }
+}
